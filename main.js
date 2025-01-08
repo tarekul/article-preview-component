73c66459cf19ea524a09ca1a97c3206432a0f50b
tarekul <rahin6810@gmail.com>
2025-01-08 02:42:37 -0500

    make share button responsive and start mobile view

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
+const body = document.body;
 const shareButton = document.getElementById('share');
 const social = document.querySelector('.social');
 
-shareButton.addEventListener('click', () => {
-    social.classList.toggle('active');
+shareButton.addEventListener('click', (event) => {
+    social.classList.add('active');
+    event.stopPropagation();
+})
+
+body.addEventListener('click', () => {
+    if (social.classList.contains('active')) {
+        social.classList.remove('active');
+    }
 })
